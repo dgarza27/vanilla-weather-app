68f08647415a6668e5a77580646b0709644c1bd8
dgarza27 <103591959+dgarza27@users.noreply.github.com>
2022-05-27 10:42:13 -0500

    updated html
    aligned degrees to the right of picture and wording

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
+function displayTemperature(response) {}
 
+let apiKey = "00a006d0f0de6b3a3c074d8ac7d952d7";
+let apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=${apiKey}&units=metric";
+
+axios.get(apiUrl).then(displayTemperature);
