4be669f7081c93d02266a71168986355b6a65756
dgarza27 <103591959+dgarza27@users.noreply.github.com>
2022-05-27 17:14:03 -0500

    update time
    not reflecting properly, further investigation.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,43 @@
-function displayTemperature(response) {}
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayTemperature(response) {
+  console.log(response.data);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.main.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.main.wind.speed;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+}
 
 let apiKey = "00a006d0f0de6b3a3c074d8ac7d952d7";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=Houston&appid=${apiKey}&units=metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
